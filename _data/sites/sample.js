2110c2ce6df4fb14cf7b63ca83472c715cb0a83f
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2021-08-02 15:24:10 -0700

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,9 +2,16 @@ module.exports = {
 	name: "dharmeshkakadia.com", // optional, falls back to object key
 	description: "dharmeshkakadia.com",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 10, // (in minutes)
 	},
 	urls: [
-		"https://dharmeshkakadia.com"
-	]
+		"https://dharmeshkakadia.com",
+		"https://dharmeshkakadia.com/blog/",
+		"https://dharmeshkakadia.com/shorts/",
+		"https://dharmeshkakadia.com/talks/",
+		"https://dharmeshkakadia.com/words/",
+		"https://dharmeshkakadia.com/book/",
+		"https://dharmeshkakadia.com/mailinglist/"
+	],
+	skip: false
 };
